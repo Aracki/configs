3d3804736cb687c6d90e6307ae20b0d057a7190a
Aracki <aracki.ivan@gmail.com>
2019-01-16 14:11:59 +0100

    Add new configs

--- .bashrc
+++ .bashrc
@@ -3,8 +3,9 @@
 # ~/.bash_aliases, instead of adding them here directly.
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 
-. ~/.config/servers
+. ~/.servers
 
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
+
--- config.fish
+++ config.fish
@@ -2,36 +2,11 @@ export GOROOT=/usr/local/go
 export GOPATH=/Users/Raca/go
 export GOBIN=$GOPATH/bin
 
-. ~/.config/servers
+. ~/.servers
 
-alias cd..='cd ..'
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
+. ~/.aliases
 
-alias h='history'
-alias a='bat'
-alias l='ls -lrth'
-alias tre='tree -L 2'
-alias s='git status'
-alias dif='git diff'
-alias publicip='curl ipinfo.io/ip'
-
-alias dps='docker ps --all --format "table {{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Ports}}"'
-alias di='docker images'
-alias dsp='docker system prune'
-alias dis='docker images --format '{{.Size}}\t{{.Repository}}' | sort -r'
-alias dn='docker network ls'
-alias dv='docker volume ls'
-alias dc="docker-compose"
-
-alias k='kubectl'
-alias ksys='k get pod --namespace kube-system'
-alias kall='k get --all-namespaces'
-alias ktx='kubectx'
-alias kns='kubens'
-alias ktl='kubetail'
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 function manopt
     set -l cmd $argv[1]
